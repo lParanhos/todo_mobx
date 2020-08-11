1c5ccfab7777e60085041d089be51a5ed3aeb571
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-11 09:15:40 -0300

    create observable todo list

---
+++ lib/stores/list_store.dart
@@ -0,0 +1,22 @@
+import 'package:mobx/mobx.dart';
+part 'list_store.g.dart';
+
+class ListStore = _ListStoreBase with _$ListStore;
+
+abstract class _ListStoreBase with Store {
+  @observable
+  String newTodoTitle = "";
+
+  @action
+  void setNewTodoTitle(String value) => newTodoTitle = value;
+
+  @computed
+  bool get isFormValid => newTodoTitle.isNotEmpty;
+
+  ObservableList<String> todoList = ObservableList<String>();
+
+  @action
+  void addTodo() {
+    todoList.add(newTodoTitle);
+  }
+}
